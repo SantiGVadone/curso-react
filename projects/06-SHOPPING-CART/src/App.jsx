1d54b6afb2a12f8b5f80dda76c63f53a9d1fe2a8
SantiGVadone <santiagovadone36@gmail.com>
2026-03-21 18:36:24 -0300

    feat: first task complete

--- projects/06-SHOPPING-CART/src/App.jsx
+++ projects/06-SHOPPING-CART/src/App.jsx
@@ -1,5 +1,8 @@
+import { products } from './mocks/products.json'
+import { Products } from './components/Products'
+
 function App() {
-  return <h1>🛒Shopping cart🛒</h1>
+  return <Products products={products} />
 }
 
 export default App
